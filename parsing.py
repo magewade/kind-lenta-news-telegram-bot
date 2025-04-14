430c09364ef7ee751e77563642923c174a7d4fd0
Marie Muravyeva <marie.muraveva@gmail.com>
2025-04-14 18:38:34 +0600

    webdriver problem fix

--- parsing.py
+++ parsing.py
@@ -121,9 +121,11 @@ def run_parsing():
 
     # Инициализация веб-драйвера
     chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument("headless")
-    chrome_options.add_argument("no-sandbox")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("disable-dev-shm-usage")
+    chrome_options.add_argument("--disable-dev-shm-usage")  # Чтобы избежать ошибок с памятью
+    chrome_options.add_argument("--disable-gpu")  # Для обхода проблем с GPU
     driver = webdriver.Chrome(options=chrome_options)
     driver.set_page_load_timeout(300)
 
